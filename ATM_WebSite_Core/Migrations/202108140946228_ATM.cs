4f17121ca47160b666e87a8060b720aeaba8e187
lexipun <oleg.zolotar.develop.@gmail.com>
2021-08-14 12:48:22 +0300

    database was created

---
+++ ATM_WebSite_Core/Migrations/202108140946228_ATM.cs
@@ -0,0 +1,61 @@
+﻿namespace ATM_WebSite_Core.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ATM : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cards",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        NumberOfCard = c.String(),
+                        PinCode = c.String(),
+                        Balance = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        UserId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Transactions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Time = c.DateTime(nullable: false),
+                        Name = c.String(),
+                        CountMoney = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        CardId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Cards", t => t.CardId, cascadeDelete: true)
+                .Index(t => t.CardId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        SurName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Cards", "UserId", "dbo.Users");
+            DropForeignKey("dbo.Transactions", "CardId", "dbo.Cards");
+            DropIndex("dbo.Transactions", new[] { "CardId" });
+            DropIndex("dbo.Cards", new[] { "UserId" });
+            DropTable("dbo.Users");
+            DropTable("dbo.Transactions");
+            DropTable("dbo.Cards");
+        }
+    }
+}
---
+++ ATM_WebSite_Core/Model/ATMContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATM_WebSite_Core.Model
+{
+    public class ATMContext: DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // configures one-to-many relationship
+            modelBuilder.Entity<Card>()
+                .HasRequired(s => s.User)
+                .WithMany(g => g.Cards)
+                .HasForeignKey(s => s.UserId);
+
+            modelBuilder.Entity<Transaction>()
+                .HasRequired(s => s.Card)
+                .WithMany(g => g.Transactions)
+                .HasForeignKey(s => s.CardId);
+        }
+    }
+
+
+}
+
